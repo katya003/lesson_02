37bb86cbfdf44434db8b903df0d57246c20d3a72
Екатерина <adelaida136666@mail.ru>
2024-09-04 18:49:24 +0300

    added getMessage() method

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,6 +2,10 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru!");
+        System.out.println(getMessag());
+    }
+    private static String getMessag(){
+        return "Hello qa.guru!";
     }
 }
+
